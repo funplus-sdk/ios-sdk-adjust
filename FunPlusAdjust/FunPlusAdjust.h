9fe118e302f7ffb030373d59c1b190e7a01b8993
yuankun.zhang <fmcocoa@gmail.com>
2016-11-04 15:34:25 +0800

    seperated from central repo

---
+++ FunPlusAdjust/FunPlusAdjust.h
@@ -0,0 +1,19 @@
+//
+//  FunPlusAdjust.h
+//  FunPlusAdjust
+//
+//  Created by Yuankun Zhang on 04/11/2016.
+//  Copyright © 2016 FunPlus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunPlusAdjust.
+FOUNDATION_EXPORT double FunPlusAdjustVersionNumber;
+
+//! Project version string for FunPlusAdjust.
+FOUNDATION_EXPORT const unsigned char FunPlusAdjustVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunPlusAdjust/PublicHeader.h>
+
+
